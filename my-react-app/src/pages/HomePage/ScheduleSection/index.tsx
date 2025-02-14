ccb137de6bf94df6fc46a7ba8849935861d39db1
João Augusto <jaugustovpires@gmail.com>
2025-02-14 00:51:20 -0300

    feat: initial schedule section complete

--- my-react-app/src/pages/HomePage/ScheduleSection/index.tsx
+++ my-react-app/src/pages/HomePage/ScheduleSection/index.tsx
@@ -4,11 +4,11 @@ import { Section } from "./style";
 export const ScheduleSection = () => {
   return (
     <Section>
-      <article className="container-content">
-        <figure className="container-content--banner">
+      <article className="container-questions">
+        <figure className="container-questions--banner">
           <img src={BannerFullDeskCongresso} alt="" />
         </figure>
-        <div className="container-content--info">
+        <div className="container-questions--info">
           <div className="info-box">
             <p>QUANDO?</p>
             <p>
@@ -34,6 +34,143 @@ export const ScheduleSection = () => {
           <li>25 MIN</li>
           <li>05 SEG</li>
         </ul>
+      </article>
+      <article className="container-plan">
+        <div className="container-plan--info">
+          <p>PROGRAMAÇÃO</p>
+          <fieldset>
+            <label htmlFor="">SELECIONE O IDIOMA</label>
+            <select name="" id="">
+              <option value="">PT</option>
+              <option value="">EN</option>
+            </select>
+          </fieldset>
+          <p>As agendas estão distribuidas por dia, horário e ambientes.</p>
+          <p>
+            **Alterações de grade referentes aos speakers e horário podem
+            ocorrer sem aviso prévio.
+          </p>
+        </div>
+        <ul className="container-plan--list">
+          <li>
+            <p>BUSINESS CLASS</p>
+            <p>QUI 10 DE ABRIL</p>
+          </li>
+          <li>
+            <p>CONGRESSO 1 DIA</p>
+            <p>SEX 11 DE ABRIL</p>
+          </li>
+          <li>
+            <p>CONGRESSO 2 DIA</p>
+            <p>SÁB 12 De ABRIL</p>
+          </li>
+        </ul>
+        <div className="container-plan--about">
+          <p>
+            **No dia 10 haverá uma Business Class voltada a médicos e
+            profissionais de saúde que administram clinicas.
+          </p>
+          <p>
+            Nos dias 11 e 12 de abril as palestras são abertas ao público,
+            escolha suas palestras e garanta seu ingresso no Congresso.
+          </p>
+        </div>
+        <div className="container-plan--content">
+          <div className="content--title">
+            <p>SALA PRINCIPAl</p>
+            <p>Clique para ver a programação da sala principal</p>
+          </div>
+          <ul className="container-content-list">
+            <li className="content-list-card">
+              <div className="card-date">
+                <p>10/04</p>
+                <p>ABERTURA</p>
+              </div>
+              <div className="card-content">
+                <p>Fabio Iwai e João Pedro Junqueira</p>
+                <p>CEO e Diretor Médico, Grupo Huntington</p>
+              </div>
+            </li>
+            <li className="content-list-card">
+              <div className="card-date">
+                <p>10/04</p>
+                <p>AULA 01</p>
+              </div>
+              <div className="card-content">
+                <p>
+                  Inovação e Exclusividade: Redefinindo a Jornada do Paciente
+                </p>
+                <p>Claudia Cohn - Diretora de Negócios Nacionais, Dasa</p>
+              </div>
+            </li>
+            <li className="content-list-card">
+              <div className="card-date">
+                <p>10/04</p>
+                <p>AULA 02</p>
+              </div>
+              <div className="card-content">
+                <p>
+                  Marketing Digital de Alta Performance para Negócios de Saúde
+                </p>
+                <p>
+                  Gustavo Santana e Dib Naim Sekkar, Sócios Diretores da Wigoo
+                </p>
+              </div>
+            </li>
+            <li className="content-list-card">
+              <div className="card-date">
+                <p>10/04</p>
+                <p>AULA 03</p>
+              </div>
+              <div className="card-content">
+                <p>
+                  Dados que Transformam: Como a Análise Estratégica Impulsiona
+                  os Negócios
+                </p>
+                <p>Daniel Marques - Data & Analytics Director; Natura </p>
+              </div>
+            </li>
+            <li className="content-list-card">
+              <div className="card-date">
+                <p>10/04</p>
+                <p>AULA 04</p>
+              </div>
+              <div className="card-content">
+                <p>
+                  Gestão Estratégica na saúde: Como Planejar o Futuro do Seu
+                  Negócio
+                </p>
+                <p>Luís Fernando Vieira Joaquim - Sócio, Deloitte</p>
+              </div>
+            </li>
+            <li className="content-list-card">
+              <div className="card-date">
+                <p>10/04</p>
+                <p>AULA 05</p>
+              </div>
+              <div className="card-content">
+                <p>
+                  Além do Consultório: Por que o Médico do Futuro Também Precisa
+                  Pensar como Empresário
+                </p>
+                <p>Ricardo Salem Ribeiro - Diretor Executivo, Care Plus</p>
+              </div>
+            </li>
+            <li className="content-list-card">
+              <div className="card-date">
+                <p>10/04</p>
+                <p>ENCERRAMENTO</p>
+              </div>
+              <div className="card-content">
+                <p>Fabio Iwai e João Pedro Junqueira</p>
+                <p>
+                  Dados que Transformam: Como a Análise Estratégica Impulsiona
+                  os Negócios
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </article>
     </Section>
   );
--- my-react-app/src/pages/HomePage/ScheduleSection/style.ts
+++ my-react-app/src/pages/HomePage/ScheduleSection/style.ts
@@ -59,4 +59,117 @@ export const Section = styled.section`
       border-radius: 100%;
     }
   }
+
+  .container-plan {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    gap: 32px;
+  }
+
+  .container-plan--info {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    gap: 32px;
+  }
+
+  .container-plan--list {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 100%;
+    gap: 32px;
+
+    li {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      width: 100%;
+      max-width: 312px;
+    }
+  }
+
+  .container-plan--about {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: start;
+
+    width: 100%;
+  }
+
+  .container-plan--content {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+
+    width: 100%;
+    padding: 16px;
+    gap: 32px;
+  }
+
+  .content-title {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    width: 100%;
+    height: 100px;
+
+    background-color: white;
+  }
+
+  .container-content-list {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: center;
+
+    width: 100%;
+    gap: 16px;
+  }
+
+  .content-list-card {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 100%;
+    gap: 128px;
+  }
+
+  .card-date {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    max-width: 80px;
+    gap: 8px;
+  }
+
+  .card-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: start;
+
+    width: 100%;
+    gap: 12px;
+  }
 `;
